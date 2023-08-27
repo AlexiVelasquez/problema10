40ff1c114f574bc95c3b97df01010501add3df62
alexi <alexi@LAPTOP-K1LI30UL>
2023-08-27 15:05:05 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Ingrese todo su consumo respectivo");
+double consumo = double.Parse(Console.ReadLine());
+double dscto, total;
+if (consumo <= 100)
+{
+    dscto = consumo * 0.10;
+    total = dscto +18/100;
+    Console.WriteLine("Total a pagar:" + total + "$");
+}
+else if (consumo >= 100)
+{
+
+    dscto = consumo * 0.20;
+    total = dscto +18/100;
+    Console.WriteLine("Total a pagar:" + total + "$");
+
+}
